0a2e744152aa709dd5708fa4847d034c3cfb1b0b
Amelia Fannin <ameliafannin@gmail.com>
2014-11-09 12:54:15 -0500

    added highlight for notable sightings

--- bird_app/views.py
+++ bird_app/views.py
@@ -10,9 +10,10 @@ gi = GeoIP(app)
 
 @app.route('/')
 def index():
-    distance = 5
+    distance = 25
     location = get_user_location()
     results = bird_count(location, distance)
+    notables = notable_sightings(location, distance)
     return render_template('bird_count.html',
                             date = time.strftime("%A, %B %d, %Y"),
                             miles = distance,
@@ -21,7 +22,8 @@ def index():
                             bird_names = sorted(results),
                             bird_counts = results,
                             user_city = location['city'],
-                            user_state = location['region_code'])
+                            user_state = location['region_code'],
+                            notable_sightings = notables)
 
 # uses ip address and GeoIP to get user location info
 def get_user_location():
@@ -29,20 +31,20 @@ def get_user_location():
 
     # subs in default ip address when running in localhost
     if user_ip == '127.0.0.1':
-        user_ip = '50.133.222.179'
+        # user_ip = '50.133.222.179'
         # for different sample, Miami, Fl
-        # user_ip = '131.94.186.10'
+        user_ip = '131.94.186.10'
 
     location = gi.record_by_addr(user_ip)
     return location
     
 # makes up the request url
-def url_for(longitude, latitude, distance):
+def all_sightings_url(longitude, latitude, distance):
     return 'http://ebird.org/ws1.1/data/obs/geo/recent?lng={longitude}&lat={latitude}&dist={distance}&back=1&maxResults=500&locale=en_US&fmt=json'.format(**locals()) 
 
 # gets report of sightings from ebird API
-def get_report(location, distance):
-    r = requests.get(url_for(location['longitude'], location['latitude'], distance))
+def get_all_report(location, distance):
+    r = requests.get(all_sightings_url(location['longitude'], location['latitude'], distance))
     return json.loads(r.text)
 
 # filters and tallies sightings reported to ebird
@@ -56,7 +58,7 @@ def bird_count(location, distance):
     false = 'false'
     true = 'true'
 
-    info = get_report(location, distance)
+    info = get_all_report(location, distance)
 
     for i in info:
         observed_day = datetime.strptime(i['obsDt'],fmt).strftime("%d")
@@ -82,3 +84,41 @@ def bird_count(location, distance):
                 count[name] = number_observed
 
     return count
+
+# Notable Sightings Methods Below
+
+
+# creates notable sightings url
+def notable_sightings_url(longitude, latitude, distance):
+    return 'http://ebird.org/ws1.1/data/notable/geo/recent?lng={longitude}&lat={latitude}&dist={distance}&back=1&maxResults=500&detail=simple&locale=en_US&fmt=json'.format(**locals())
+
+# gets report of sightings from ebird API
+def get_notables_report(location, distance):
+    r = requests.get(notable_sightings_url(location['longitude'], location['latitude'], distance))
+    return json.loads(r.text)
+    
+# makes list of notable species names
+def notable_sightings(location, distance):
+    notables = []
+
+    fmt = "%Y-%m-%d %H:%M"
+    current_day = time.strftime("%d")
+
+    # handles NameError issue with info from e-bird API
+    false = 'false'
+    true = 'true'
+
+    info = get_notables_report(location, distance)
+
+    for i in info:
+        observed_day = datetime.strptime(i['obsDt'],fmt).strftime("%d")
+
+        # ensures only today's sightings
+        if observed_day == current_day:
+            name = i['comName']
+            notables.append(name)
+    return notables
+
+
+
+
