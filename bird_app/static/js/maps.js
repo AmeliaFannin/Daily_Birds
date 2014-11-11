08d88381b1a5d7bc161563682219c6ec1d9698fe
Amelia Fannin <ameliafannin@gmail.com>
2014-11-11 18:18:39 -0500

    added mapping of notable sightings

---
+++ bird_app/static/js/maps.js
@@ -0,0 +1,7 @@
+
+// moved all this into the html for now
+
+// L.mapbox.accessToken = 'pk.eyJ1IjoiYW1lbGlhZiIsImEiOiI5OEtpVlZFIn0.81iIjFOqtIlhizjG27i0aw';
+
+// var map = L.mapbox.map('map', 'ameliaf.k6al1pfj')
+//   .setView(coordinates, 8);
--- bird_app/views.py
+++ bird_app/views.py
@@ -10,20 +10,21 @@ gi = GeoIP(app)
 
 @app.route('/')
 def index():
-    distance = 25
+    distance = 10
     location = get_user_location()
     results = bird_count(location, distance)
     notables = notable_sightings(location, distance)
-    return render_template('bird_count.html',
+    return render_template('count.html',
+                            coordinates = json.dumps([location['latitude'], location['longitude']]), 
                             date = time.strftime("%A, %B %d, %Y"),
                             miles = distance,
                             lat = round(location['latitude'], 2),
-                            long = round(location['longitude'], 2),
+                            lon = round(location['longitude'], 2),
                             bird_names = sorted(results),
                             bird_counts = results,
                             user_city = location['city'],
                             user_state = location['region_code'],
-                            notable_sightings = notables)
+                            notable_sightings = json.dumps(notables))
 
 # uses ip address and GeoIP to get user location info
 def get_user_location():
@@ -31,16 +32,16 @@ def get_user_location():
 
     # subs in default ip address when running in localhost
     if user_ip == '127.0.0.1':
-        # user_ip = '50.133.222.179'
+        user_ip = '50.133.222.179'
         # for different sample, Miami, Fl
-        user_ip = '131.94.186.10'
+        # user_ip = '131.94.186.10'
 
     location = gi.record_by_addr(user_ip)
     return location
     
 # makes up the request url
 def all_sightings_url(longitude, latitude, distance):
-    return 'http://ebird.org/ws1.1/data/obs/geo/recent?lng={longitude}&lat={latitude}&dist={distance}&back=1&maxResults=500&locale=en_US&fmt=json'.format(**locals()) 
+    return 'http://ebird.org/ws1.1/data/obs/geo/recent?lng={longitude}&lat={latitude}&dist={distance}&back=1&maxResults=500&locale=en_US&fmt=json&includeProvisional=true'.format(**locals()) 
 
 # gets report of sightings from ebird API
 def get_all_report(location, distance):
@@ -49,7 +50,10 @@ def get_all_report(location, distance):
 
 # filters and tallies sightings reported to ebird
 def bird_count(location, distance):
-    count = dict()
+    # count = dict()
+
+    # fakes in notable sightings
+    count = { 'African Swallow':1, 'European Swallow':2 }
 
     fmt = "%Y-%m-%d %H:%M"
     current_day = time.strftime("%d")
@@ -85,8 +89,13 @@ def bird_count(location, distance):
 
     return count
 
-# Notable Sightings Methods Below
 
+
+
+
+
+
+# Notable Sightings Methods 
 
 # creates notable sightings url
 def notable_sightings_url(longitude, latitude, distance):
@@ -96,10 +105,12 @@ def notable_sightings_url(longitude, latitude, distance):
 def get_notables_report(location, distance):
     r = requests.get(notable_sightings_url(location['longitude'], location['latitude'], distance))
     return json.loads(r.text)
-    
-# makes list of notable species names
+
+# makes dict of notable species names and location info
 def notable_sightings(location, distance):
-    notables = []
+    # notables = dict()
+    # fakes in notable sightings
+    notables = { 'African Swallow':['Mt. Auburn Cemetery', 42.3706, -71.1458], 'European Swallow':['Harvard Yard', 42.3745, -71.1172] }
 
     fmt = "%Y-%m-%d %H:%M"
     current_day = time.strftime("%d")
@@ -111,12 +122,18 @@ def notable_sightings(location, distance):
     info = get_notables_report(location, distance)
 
     for i in info:
-        observed_day = datetime.strptime(i['obsDt'],fmt).strftime("%d")
 
         # ensures only today's sightings
+        observed_day = datetime.strptime(i['obsDt'],fmt).strftime("%d")
         if observed_day == current_day:
-            name = i['comName']
-            notables.append(name)
+
+            if i['locName']:
+                location_name = i['locName']
+            else:
+                location_name = "unnamed location"
+
+            notables[i['comName']] = [location_name, i['lat'], i['lng']]
+            
     return notables
 
 
