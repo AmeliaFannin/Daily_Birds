b2082142066594d6aa032b4413f98e761abc6575
Amelia Fannin <ameliafannin@gmail.com>
2014-12-09 12:21:29 -0500

    more tries for heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn server:app --timeout 10
+web: gunicorn server:app 
--- server.py
+++ server.py
@@ -8,3 +8,8 @@ app.run(debug=False)
 # if __name__ == "__main__":
 #     port = int(os.environ.get("PORT", 5000))
 #     app.run(host='0.0.0.0', port=port)
+
+if __name__ == '__main__':
+# Bind to PORT if defined, otherwise default to 5000.
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
