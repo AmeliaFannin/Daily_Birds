31f675f6702615c2e7d5a32b48a1a2c0a9cd9812
Amelia Fannin <ameliafannin@gmail.com>
2014-12-10 09:16:15 -0500

    removes app.run redundancy

--- server.py
+++ server.py
@@ -1,15 +1,12 @@
-import os
 from flask import Flask
 from bird_app import app
 
 
-app.run(debug=False)
+# app.run(debug=False)
 
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
 # Bind to PORT if defined, otherwise default to 5000.
-  port = int(os.environ.get('PORT', 33507))
+  import os
+  port = int(os.environ.get('PORT', 5000))
   app.run(host='0.0.0.0', port=port)
